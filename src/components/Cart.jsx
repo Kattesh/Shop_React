883871e0a0216420274913e65320c9363e678914
Kattesh <kate.sh@icloud.com>
2023-01-04 19:02:15 -0600

    cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Cart = (props) => {
-    const {} = props
+    const {quantity = 0} = props
     return (
-        <div>
-
+        <div className='cart teal darken-2 white-text'>
+            <i className="material-icons">shopping_cart</i>
+            {quantity? <span className='cart-quantity'>{quantity}</span>:null}
         </div>
     );
 };
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,11 +2,13 @@ import React, {useEffect, useState} from 'react';
 import {API_KEY, API_URL} from "../config";
 import Preloader from "./Preloader";
 import GoodsList from "./GoodsList";
+import Cart from "./Cart";
 
 
 const Shop = () => {
     const [goods, setGoods] = useState([])
     const [loading, setLoading] = useState(true)
+    const [order, setOrder] = useState([])
 
     useEffect(function getGoods() {
         fetch(API_URL, {
@@ -23,6 +25,7 @@ const Shop = () => {
 
     return (
         <main className='container content'>
+            <Cart quantity={order.length}/>
             {loading ? <Preloader/> : <GoodsList goods={goods}/>}
         </main>
     );
